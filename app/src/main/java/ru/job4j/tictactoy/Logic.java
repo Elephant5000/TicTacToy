30693ec5f675d1c84d28b68955693d806fb930c8
Elephant5000 <pashau@mail.ru>
2020-09-18 19:16:14 +0300

    Создан обработчик нажатия клавишь и отрисовки игрового поля

--- app/src/main/java/ru/job4j/tictactoy/Logic.java
+++ app/src/main/java/ru/job4j/tictactoy/Logic.java
@@ -13,7 +13,34 @@ public class Logic {
     private boolean playerPC = false;
     private byte[][] board = {{0,0,0}, {0,0,0}, {0,0,0}};
 
+    public boolean isWhoseMove() {
+        return whoseMove;
+    }
 
+    public void setWhoseMove(boolean whoseMove) {
+        this.whoseMove = whoseMove;
+    }
+
+    public boolean isPlayerPC() {
+        return playerPC;
+    }
+
+    public void setPlayerPC(boolean playerPC) {
+        this.playerPC = playerPC;
+    }
+
+    public byte[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(byte[][] board) {
+        this.board = board;
+    }
+
+    public void setMove(int move) {
+        this.board[move / 3][move % 3] = (byte) (whoseMove ? 2 : 1);
+        this.whoseMove = !this.whoseMove;
+    }
 
 
 }
--- app/src/main/java/ru/job4j/tictactoy/MainActivity.java
+++ app/src/main/java/ru/job4j/tictactoy/MainActivity.java
@@ -3,12 +3,41 @@ package ru.job4j.tictactoy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Logic game = new Logic();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        paintBoard(game);
     }
+
+    public void onButtonClick(View view) {
+        String g =  getResources().getResourceName(view.getId());
+        g = g.substring(g.length()-1);
+        int i = Integer.parseInt(g);
+        game.setMove(i);
+        paintBoard(game);
+    }
+
+    public void paintBoard(Logic board) {
+        for (int index = 0; index <= 8; index++) {
+            Button button = findViewById(getResources().getIdentifier("button" + String.valueOf(index), "id", this.getPackageName()));
+            if (board.getBoard()[index / 3][index % 3] == 0) {
+                button.setText("");
+            } else if (board.getBoard()[index / 3][index % 3] == 1) {
+                button.setText("X");
+            } else if (board.getBoard()[index / 3][index % 3] == 2) {
+                button.setText("O");
+            } else {
+                button.setText("E");
+            }
+        }
+    };
+
 }
