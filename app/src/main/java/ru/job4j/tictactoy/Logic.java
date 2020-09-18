c739f43c3e355ef0d569ef5cbe531ea3e4c6705e
Elephant5000 <pashau@mail.ru>
2020-09-18 17:01:27 +0300

    Создан класс Logic

---
+++ app/src/main/java/ru/job4j/tictactoy/Logic.java
@@ -0,0 +1,19 @@
+package ru.job4j.tictactoy;
+
+/**
+ * Класс реализующий логику игры крестики нолики
+ * whoseMove - чей ход. false - первый игрок, true - второй
+ * playerPC - с кем играем. false - c человеком, true - с компьютером
+ * board - игровое поле. 0 - пусто. 1 - крестик, 2 - нолик.
+
+ */
+
+public class Logic {
+    private boolean whoseMove = false;
+    private boolean playerPC = false;
+    private byte[][] board = {{0,0,0}, {0,0,0}, {0,0,0}};
+
+
+
+
+}
