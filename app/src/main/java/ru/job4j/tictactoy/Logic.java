7b0e339fa8cc6faee99499e32160ef5b52b6ab37
Elephant5000 <pashau@mail.ru>
2020-09-21 07:14:50 +0300

    Перешел на одномерный массив

--- app/src/main/java/ru/job4j/tictactoy/Logic.java
+++ app/src/main/java/ru/job4j/tictactoy/Logic.java
@@ -1,5 +1,7 @@
 package ru.job4j.tictactoy;
 
+import android.widget.Toast;
+
 /**
  * Класс реализующий логику игры крестики нолики
  * whoseMove - чей ход. false - первый игрок, true - второй
@@ -11,7 +13,8 @@ package ru.job4j.tictactoy;
 public class Logic {
     private boolean whoseMove = false;
     private boolean playerPC = false;
-    private byte[][] board = {{0,0,0}, {0,0,0}, {0,0,0}};
+    private byte[] board = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 
     public boolean isWhoseMove() {
         return whoseMove;
@@ -29,18 +32,61 @@ public class Logic {
         this.playerPC = playerPC;
     }
 
-    public byte[][] getBoard() {
+    public byte[] getBoard() {
         return board;
     }
 
-    public void setBoard(byte[][] board) {
+    public void setBoard(byte[] board) {
         this.board = board;
     }
 
     public void setMove(int move) {
-        this.board[move / 3][move % 3] = (byte) (whoseMove ? 2 : 1);
+        this.board[move] = (byte) (whoseMove ? 2 : 1);
         this.whoseMove = !this.whoseMove;
     }
 
+    public byte checkWin() {
+        byte win = 0;
+        if (this.board[0] != 0 && this.board[0] == this.board[1] && this.board[0] == this.board[2])  {
+            win = this.board[0];
+            return win;
+        }
+        if (this.board[3] != 0 && this.board[3] == this.board[4] && this.board[3] == this.board[5])  {
+            win = this.board[3];
+            return win;
+        }
+        if (this.board[6] != 0 && this.board[6] == this.board[7] && this.board[6] == this.board[8])  {
+            win = this.board[6];
+            return win;
+        }
+        if (this.board[0] != 0 && this.board[0] == this.board[3] && this.board[0] == this.board[6])  {
+            win = this.board[0];
+            return win;
+        }
+        if (this.board[1] != 0 && this.board[1] == this.board[4] && this.board[1] == this.board[7])  {
+            win = this.board[1];
+            return win;
+        }
+        if (this.board[2] != 0 && this.board[2] == this.board[5] && this.board[2] == this.board[8])  {
+            win = this.board[2];
+            return win;
+        }
+        if (this.board[0] != 0 && this.board[0] == this.board[4] && this.board[0] == this.board[8])  {
+            win = this.board[0];
+            return win;
+        }
+        if (this.board[2] != 0 && this.board[2] == this.board[4] && this.board[2] == this.board[4])  {
+            win = this.board[2];
+            return win;
+        }
+        return win;
+    }
+
+    public void newGame() {
+        this.whoseMove = false;
+        for (int index = 0; index <9; index ++) {
+            this.board[index] = 0;
+        }
+    }
 
 }
--- app/src/main/java/ru/job4j/tictactoy/MainActivity.java
+++ app/src/main/java/ru/job4j/tictactoy/MainActivity.java
@@ -1,10 +1,12 @@
 package ru.job4j.tictactoy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,30 +16,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            game.setBoard(savedInstanceState.getByteArray("GameBoard"));
+            game.setWhoseMove(savedInstanceState.getBoolean("WhoseMove"));
+            game.setWhoseMove(savedInstanceState.getBoolean("PlayerPC"));
+        }
         paintBoard(game);
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putByteArray("GameBoard", game.getBoard());
+        outState.putBoolean("WhoseMove", game.isWhoseMove());
+        outState.putBoolean("PlayerPC", game.isPlayerPC());
+    }
+
+    /**
+     * Обработчик нажатия кнопок
+     * Все кнопки имеют заполненное поле tag заполненное соответствующим значением от 0 до 8.
+     * Передаем это значение в метод game.setMove(int index) и перерисовываем игровое поле.
+     * @param view
+     */
+
     public void onButtonClick(View view) {
-        String g =  getResources().getResourceName(view.getId());
-        g = g.substring(g.length()-1);
-        int i = Integer.parseInt(g);
-        game.setMove(i);
-        paintBoard(game);
+        int buttonIndex = Integer.parseInt(view.getTag().toString());
+        if (game.getBoard()[buttonIndex] == 0) {
+            game.setMove(buttonIndex);
+            paintBoard(game);
+            if (game.checkWin() > 0) {
+                endGame();
+            }
+        }
     }
 
     public void paintBoard(Logic board) {
         for (int index = 0; index <= 8; index++) {
             Button button = findViewById(getResources().getIdentifier("button" + String.valueOf(index), "id", this.getPackageName()));
-            if (board.getBoard()[index / 3][index % 3] == 0) {
+            if (board.getBoard()[index] == 0) {
                 button.setText("");
-            } else if (board.getBoard()[index / 3][index % 3] == 1) {
+            } else if (board.getBoard()[index] == 1) {
                 button.setText("X");
-            } else if (board.getBoard()[index / 3][index % 3] == 2) {
+            } else if (board.getBoard()[index] == 2) {
                 button.setText("O");
             } else {
                 button.setText("E");
             }
         }
-    };
+    }
+
+    public void endGame() {
+        game.newGame();
+    }
 
 }
